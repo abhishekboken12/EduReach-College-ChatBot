1193334ca26d86f8b2c4ab84eecec9ee11a6a9c5
abhishekboken12 <abhishekboken12@gmail.com>
2026-08-31 07:46:12 +0530

    Changes Node env from production to Production

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,8 +6,6 @@ import errorHandler from "./middleware/error-handler.middleware.ts";
 
 const app: Application = express();
 
-
-
 app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
